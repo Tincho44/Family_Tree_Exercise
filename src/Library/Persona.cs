e5eebbb6f11b11e8de80a9a2705818644ab6d9fc
Tincho44 <martinharbuet@gmail.com>
2022-11-14 03:53:38 -0300

    Se cambia la clase Persona

---
+++ src/Library/Persona.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+
+namespace Library
+{
+
+    public class Persona
+    {
+
+        public string nombre;
+        public int edad;
+
+        public Persona (string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public int Edad
+        {
+
+            get
+            {
+                return this.edad;
+            }
+
+            set
+            {
+                this.edad = value;
+            }
+        }
+    }
+}
